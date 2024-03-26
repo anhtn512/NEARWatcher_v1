b0ff5badcf79d268c991481d6aec3ce06a45eb84
anhtn512 <anhtn512@gmail.com>
2024-03-26 20:42:35 +0700

    create util and db function

---
+++ db.js
@@ -0,0 +1,73 @@
+const { Level } = require('level')
+const addresses = new Level('./db/addresses')
+const rooms = new Level('./db/rooms')
+
+async function getAllAddresses() {
+  let list = []
+  for await (const key of addresses.keys()) {
+    list.push(key)
+  }
+  return list
+}
+
+function serializeObject(obj) {
+  return JSON.stringify(obj)
+}
+
+function deserializeObject(obj) {
+  return JSON.parse(obj)
+}
+
+async function getAddresses(roomId) {
+  try {
+    return deserializeObject(await rooms.get(roomId))
+  } catch { }
+  return []
+}
+
+async function getRooms(address) {
+  try {
+    return deserializeObject(await addresses.get(address))
+  } catch { }
+  return []
+}
+
+async function addRoom(address, roomId) { //add room to address
+  let list = await getRooms(address)
+  if (list.indexOf(roomId) === -1) {
+    list.push(roomId)
+    await addresses.put(address, serializeObject(list))
+  }
+}
+
+async function addAddress(roomId, address) { //add address to room
+  let list = await getAddresses(roomId)
+  if (list.indexOf(address) === -1) {
+    list.push(address)
+    await rooms.put(roomId, serializeObject(list))
+    addRoom(address, roomId)
+    return true
+  }
+  return false
+}
+
+async function removeAddress(roomId, address) {
+  let list = await getAddresses(roomId)
+  if (list.indexOf(address) === -1) return false
+  list = list.filter(a => a !== address)
+  await rooms.put(roomId, serializeObject(list))
+
+  list = await getRooms(address)
+  list = list.filter(a => a !== roomId)
+  if (list.length === 0) addresses.del(address)
+  else await addresses.put(address, serializeObject(list))
+  return true
+}
+
+module.exports = {
+  addAddress,
+  removeAddress,
+  getAddresses,
+  getRooms,
+  getAllAddresses
+}
---
+++ util.js
@@ -0,0 +1,47 @@
+const { escapers } = require('@telegraf/entity')
+
+function extractActions(actions) {
+  let tempActions = actions.map(action => {
+    const key = Object.keys(action)[0]
+    return { type: key, ...action[key] }
+  })
+  return tempActions.map(action => decodeAction(action))
+}
+
+function decodeAction(action) {
+  if (action.type === 'FunctionCall') {
+    const temp = Buffer.from(action.args, 'base64')
+    try {
+      action.args = JSON.parse(String.fromCharCode(...temp))
+    } catch {
+      action.args = "Can't decode it"
+    }
+    return action
+  } else if (action.type === 'Delegate') {
+    const actions = extractActions(action.delegate_action.actions)
+    action.delegate_action.actions = actions
+    return action
+  }
+  return action
+
+}
+
+function txToString(tx) {
+  const actions = extractActions(tx.actions)
+  let s = `sender: *${escapers.MarkdownV2(tx.sender)}*
+receiver: *${escapers.MarkdownV2(tx.receiver)}*
+hash: *${escapers.MarkdownV2(tx.hash)}*
+action: 
+`;
+  actions.forEach(action => {
+    s += `\`${escapers.MarkdownV2(JSON.stringify(action, null, 2))}\`
+`
+  })
+  return s
+}
+
+module.exports = {
+  convertTimestamp,
+  extractActions,
+  txToString
+};
